ba01a4111e430ddc33074a67b8fa2233c8ff2655
sajad shokraei <shockzbeatbox@gmail.com>
2024-10-31 01:39:06 +0330

    update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import { CategoryProvider } from "./context/CategoryContext";
 
 function App() {
   return (
-    <Router>
+    
       <CategoryProvider>
         <Routes>
           <Route path="/" element={<HomePage />} />
         </Routes>
       </CategoryProvider>
-    </Router>
+    
   );
 }
 
